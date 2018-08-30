d1d4f598fbabe410c61c111e6497c922add72f64
Christopher Dengsø <chrdengso@gmail.com>
2018-08-30 21:33:37 +0800

    error handling

--- src/api/routes/v1/analyzer.route.js
+++ src/api/routes/v1/analyzer.route.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { analyze } = require('../../services/lighthouse');
 const { handler: errorHandler } = require('../../middlewares/error');
+const APIError = require('../../utils/APIError');
+const httpStatus = require('http-status');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const { url } = req.body;
   try {
+    const { url } = req.body;
+    if (!url) {
+      throw new APIError({
+        message: 'Provide a url in the post body',
+        status: httpStatus.BAD_REQUEST,
+        isPublic: true,
+      });
+    }
     return await analyze(url).then(data => res.json(data));
   } catch (error) {
     return errorHandler(error, req, res);
